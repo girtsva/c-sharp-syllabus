f62f67f61b10619b6b5afa18a436117410ddf463
Girts Varna <gvarna@gmail.com>
2021-11-28 17:08:42 +0200

    Exercise07 completed

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise07/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that prompts the user to enter a string and displays the number of the uppercase letters in the string.
+
+            Console.WriteLine("You will be asked to enter a string of characters.");
+            Console.WriteLine("You can use uppercase and lowercase letters and any other characters.");
+            Console.WriteLine("The program will show you the number of uppercase letters in your string.");
+            Console.WriteLine("Please enter your string:");
+            string userString = Console.ReadLine();
+
+            int counter = 0;
+
+            for (int i = 0; i < userString.Length; i++)
+            {
+                if (char.IsUpper(userString[i]))
+                {
+                    counter ++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"You have entered {counter} uppercase letters in your string.");
+        }
+    }
+}
