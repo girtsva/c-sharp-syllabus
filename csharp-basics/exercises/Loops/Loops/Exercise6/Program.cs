cf9993cd822bc08cd93aac34975587f515d12282
Girts Varna <gvarna@gmail.com>
2021-12-07 13:52:58 +0200

    Exercise6 completed

---
+++ csharp-basics/exercises/Loops/Loops/Exercise6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the maximum integer value: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= userNumber; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                }
+
+                if (i % 20 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
