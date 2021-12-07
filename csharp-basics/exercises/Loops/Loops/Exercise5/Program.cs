6192e86c468b2d147fb78c4a381d5841dbec44fb
Girts Varna <gvarna@gmail.com>
2021-12-07 13:40:16 +0200

    Exercise5 completed

---
+++ csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You are asked to enter two words.");
+            Console.Write("Please enter the first word: ");
+            string firstWord = Console.ReadLine();
+            Console.Write("Please enter the second word: ");
+            string secondWord = Console.ReadLine();
+
+            string dots = String.Empty;
+
+            for (int i = 0; i < 30 - firstWord.Length - secondWord.Length; i++)
+            {
+                dots += ".";
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(firstWord + dots + secondWord);
+        }
+    }
+}
