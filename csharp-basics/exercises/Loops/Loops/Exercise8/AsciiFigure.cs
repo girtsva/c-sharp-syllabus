f93fb44bf3e94802b7a4a5a0b22c998e31ea78ec
Girts Varna <gvarna@gmail.com>
2021-12-08 15:20:38 +0200

    Exercise8 completed

---
+++ csharp-basics/exercises/Loops/Loops/Exercise8/AsciiFigure.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class AsciiFigure
+    {
+        static void Main(string[] args)
+        {
+            const int numberOfRows = 5;
+
+            double rowLength = (numberOfRows - 3)/2.0 * 16 + 16;
+            double midline = rowLength / 2;
+            int stepLength = 4;
+
+            for (int rowInd = 0; rowInd < numberOfRows; rowInd++)
+            {
+                string row = "";
+
+                for (int colInd = 0; colInd < rowLength; colInd++)
+                {                    
+                    if (colInd < midline - stepLength * rowInd)
+                    {
+                        row += "/";
+                    }
+                    else if (colInd >= midline + stepLength * rowInd)
+                    {
+                        row += "\\";
+                    }
+                    else 
+                    {
+                        row += "*";
+                    }
+                }
+
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
