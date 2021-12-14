0f73633c9a0986e3529598f7d174b062fe3d9395
Girts Varna <gvarna@gmail.com>
2021-12-14 13:41:32 +0200

    Exercise03 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise03/Odometer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    public class Odometer
+    {
+        private int _maxMileage = 999999;
+        private int _startingMileage;
+        public int Mileage { get; private set; }
+        private FuelGauge _fuelGauge;
+        private int _fuelEconomy = 10;
+
+        public Odometer(int startingMileage, FuelGauge fuelGauge)
+        {
+            _startingMileage = startingMileage;
+            Mileage = startingMileage;
+            _fuelGauge = fuelGauge;
+        }
+
+        public int AddMileage()
+        {
+            int newMileage = Mileage < _maxMileage ? ++Mileage : 0;
+            DecreaseFuel();
+            return newMileage;
+        }
+
+        public void DecreaseFuel()
+        {
+            int distance = Mileage - _startingMileage;
+            if (distance % _fuelEconomy == 0)
+            {
+                _fuelGauge.BurnFuel();
+            }
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FuelGauge fuelIndicator = new FuelGauge();
+            Odometer mileometer = new Odometer(100, fuelIndicator);
+
+            for (int i = 0; i < fuelIndicator.GetTankCapacity(); i++)
+            {
+                fuelIndicator.AddFuel();
+            }
+
+            while(fuelIndicator.AmountOfFuel > 0)
+            {
+                int mileage = mileometer.AddMileage();
+
+                Console.WriteLine($"Car's current mileage is {mileage}");
+                Console.WriteLine($"Car's current amount of fuel is {fuelIndicator.AmountOfFuel}");
+                Console.WriteLine("***********************************");
+            }
+
+        }
+    }
+}
