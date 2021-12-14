b0c91afec73eb3f5d55f813999480bdbc39a7112
Girts Varna <gvarna@gmail.com>
2021-12-14 16:12:48 +0200

    Exercise05 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise05/Date.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise05
+{
+    class Date
+    {
+        public int Day { get; private set; }
+        public int Month { get; private set; }
+        public int Year { get; private set; }
+
+        public Date(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public string DisplayDate()
+        {
+            return $"{Day}/{Month}/{Year}";
+        }
+    }
+}
