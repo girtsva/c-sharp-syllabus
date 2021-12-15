5cb3ba334da7f3ee8df3e37a5e0732d3e630fe41
Girts Varna <gvarna@gmail.com>
2021-12-15 18:04:44 +0200

    Exercise10 (BankAccount) completed

--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -10,6 +10,13 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+            BankAccount benben = new BankAccount("Benson", 17.25);
+            BankAccount benbenMinus = new BankAccount("Benson Broke", -17.5);
+
+            Console.WriteLine(benben.ShowUserNameAndBalance());
+            Console.WriteLine(benbenMinus.ShowUserNameAndBalance());
+
+            Console.ReadKey();
         }
     }
 }
