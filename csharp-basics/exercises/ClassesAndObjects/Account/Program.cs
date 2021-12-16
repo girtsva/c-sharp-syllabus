9d2b32421332168849d397fe8dbf3798e415b3a9
Girts Varna <gvarna@gmail.com>
2021-12-16 18:03:19 +0200

    Exercise12 (Account) completed

--- csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
@@ -10,6 +10,50 @@ namespace Account
     {
         private static void Main(string[] args)
         {
+            Account firstAccount = new Account("Barto's account", 100.00);
+
+            Console.WriteLine("Initial State");
+            Console.WriteLine(firstAccount);
+
+            firstAccount.Deposit(20.00);
+
+            Console.WriteLine("Final State");
+            Console.WriteLine(firstAccount);
+            Console.WriteLine("************************");
+
+            Account mattsAcount = new Account("Matt's account", 1000.00);
+            Account myAccount = new Account("My account", 0);
+            mattsAcount.Withdrawal(100.00);
+            myAccount.Deposit(100.00);
+
+            Console.WriteLine(mattsAcount);
+            Console.WriteLine(myAccount);
+            Console.WriteLine("************************");
+
+            Account a = new Account("A", 100.00);
+            Account b = new Account("B", 0);
+            Account c = new Account("C", 0);
+
+            Console.WriteLine("Initial State");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Transfer(a, b, 50.00);
+            Transfer(b, c, 25.00);
+
+            Console.WriteLine("Final State");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+
+        public static void Transfer(Account from, Account to, double howMuch)
+        {
+            from.Withdrawal(howMuch);
+            to.Deposit(howMuch);
         }
     }
 }
