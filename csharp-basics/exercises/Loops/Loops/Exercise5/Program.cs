c51740f4d14d19a89a19d182355c5048050b8cc1
Girts Varna <gvarna@gmail.com>
2021-12-16 19:30:27 +0200

    Exercise5 updated according to requested changes

--- csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
@@ -6,13 +6,13 @@ namespace Exercise5
     {
         static void Main(string[] args)
         {
+            string dots = String.Empty;
+
             Console.WriteLine("You are asked to enter two words.");
             Console.Write("Please enter the first word: ");
             string firstWord = Console.ReadLine();
             Console.Write("Please enter the second word: ");
             string secondWord = Console.ReadLine();
-
-            string dots = String.Empty;
 
             for (int i = 0; i < 30 - firstWord.Length - secondWord.Length; i++)
             {
