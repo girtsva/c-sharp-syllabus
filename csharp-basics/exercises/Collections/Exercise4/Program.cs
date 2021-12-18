9e98142d30c252e06ab1f88ec8ec37d7066350a5
Girts Varna <gvarna@gmail.com>
2021-12-18 16:48:06 +0200

    Exercise4 completed

---
+++ csharp-basics/exercises/Collections/Exercise4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            string input = Console.ReadLine();
+            int number;
+
+            while(!int.TryParse(input, out number))
+            {
+                Console.Write("This is not an integer! Enter again: ");
+                input = Console.ReadLine();
+            }
+
+            while (number != 1 && number != 4)
+            {
+                string str = number.ToString();
+                List<int> digits = new List<int>();
+
+                foreach (char digit in str)
+                {
+                    digits.Add(Convert.ToInt32(digit.ToString()));
+                }
+
+                number = 0;
+                for (int i = 0; i < digits.Count; i++)
+                {
+                    digits[i]*= digits[i];
+                    number += digits[i];
+                }
+            }
+            
+            if (number == 1)
+            {
+                Console.WriteLine($"Integer {input} is \"happy\", as summing the squares of its digits eventually leads to {number}!");
+            }
+            else
+            {
+                Console.WriteLine($"Integer {input} is \"NOT happy\", as summing the squares of its digits eventually leads to {number}!");
+            }
+        }
+    }
+}
