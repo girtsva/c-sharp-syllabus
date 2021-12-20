a445e05619b758a881f6ce1dd82b3446dbf7e2db
Girts Varna <gvarna@gmail.com>
2021-12-20 16:33:20 +0200

    Exercise9 completed

---
+++ csharp-basics/exercises/Collections/Exercise9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashset = new HashSet<string>();
+
+            hashset.Add("One");
+            hashset.Add("Two");
+            hashset.Add("Three");
+            hashset.Add("Four");
+            hashset.Add("Five");
+
+            foreach (var item in hashset)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("----------");
+
+            hashset.Clear();
+
+            hashset.Add("Four");
+            hashset.Add("Four");
+            hashset.Add("Four");
+
+            foreach (var item in hashset)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
