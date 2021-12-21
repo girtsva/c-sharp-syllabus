7a60988b6f09b6558ee73f9195f50e25e4dd6fc3
Girts Varna <gvarna@gmail.com>
2021-12-21 17:37:43 +0200

    Exercise4 (Firm) completed

---
+++ csharp-basics/exercises/Polymorphism/Firm/Commission.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Firm
+{
+    public class Commission : Hourly
+    {
+        private double _totalSales;
+        private double _commissionRate;
+        
+        public Commission(string eName, string eAddress, string ePhone, string socSecNumber, double rate, double comissionRate) : base(eName, eAddress, ePhone, socSecNumber, rate)
+        {
+            _totalSales = 0;
+            _commissionRate = comissionRate;
+        }
+
+        public void AddSales(double totalSales)
+        {
+            _totalSales += totalSales;
+        }
+
+        public override double Pay()
+        {
+            var payment = base.Pay() + _totalSales * _commissionRate;
+            _totalSales = 0;
+            return payment;
+        }
+
+        public override string ToString()
+        {
+            var result = base.ToString();
+            result += "\nTotal sales: " + _totalSales;
+            return result;
+        }
+    }
+}
