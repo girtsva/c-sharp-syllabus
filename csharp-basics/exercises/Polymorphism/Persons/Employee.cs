32572393aae3db89ae32916ad25a7d1a4c78f436
Girts Varna <gvarna@gmail.com>
2021-12-21 16:23:31 +0200

    Exercise3 (Persons) completed

---
+++ csharp-basics/exercises/Polymorphism/Persons/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Persons
+{
+    class Employee : Person
+    {
+        protected string JobTitle { get; set; }
+
+        public Employee(string firstName, string lastName, string address, int id, string jobTitle) : base(firstName, lastName, address, id)
+        {
+            JobTitle = jobTitle;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("----------Employee Information----------");
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Job Title: {JobTitle}");
+            Console.WriteLine("----------------------------------------");
+        }
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/Persons/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Persons
+{
+    public class Student : Person
+    {
+        protected double GPA { get; set; }
+
+        public Student(string firstName, string lastName, string address, int id, double gpa) : base(firstName, lastName, address, id)
+        {
+            GPA = gpa;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("----------Student Information-----------");
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"GPA: {GPA}");
+            Console.WriteLine("----------------------------------------");
+        }
+    }
+}
