d04c65ed83559cacc13383c362053b923453434d
Girts Varna <gvarna@gmail.com>
2021-12-21 14:46:56 +0200

    Exercise2 (MakeSounds) completed

---
+++ csharp-basics/exercises/Polymorphism/MakeSounds/Parrot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MakeSounds
+{
+    public class Parrot : ISound
+    {
+        public void PlaySound()
+        {
+            Console.WriteLine("Gimme-ee co-o-okie!!");
+        }
+    }
+}
