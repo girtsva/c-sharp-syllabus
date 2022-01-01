a9af752b58aef55113a91e54bb3adbc48b7d3293
Girts Varna <gvarna@gmail.com>
2022-01-01 22:42:18 +0200

    Arithmetics Exercise06 Tests completed

---
+++ csharp-basics/exercises/Arithmetic/Exercise06/CozaLozaWoza.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Exercise06
+{
+    public class CozaLozaWoza
+    {
+
+
+        public void PrintResult(int startNumber, int endNumber, int numbersPerLine)
+        {
+            string row = "";
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                row += TransformNumber(i) + " ";
+
+                if (i % numbersPerLine == 0)
+                {
+                    Console.WriteLine(row);
+                    row = "";
+                }
+            }
+
+            Console.WriteLine(row);
+        }
+
+        public string TransformNumber(int number)
+        {
+            string transformed;
+
+            if (number % 105 == 0)
+            {
+                transformed = "CozaLozaWoza";
+            }
+            else if (number % 35 == 0)
+            {
+                transformed = "LozaWoza";
+            }
+            else if (number % 21 == 0)
+            {
+                transformed = "CozaWoza";
+            }
+            else if (number % 15 == 0)
+            {
+                transformed = "CozaLoza";
+            }
+            else if (number % 7 == 0)
+            {
+                transformed = "Woza";
+            }
+            else if (number % 5 == 0)
+            {
+                transformed = "Loza";
+            }
+            else if (number % 3 == 0)
+            {
+                transformed = "Coza";
+            }
+            else
+            {
+                transformed = number.ToString();
+            }
+
+            return transformed;
+        }
+
+    }
+}
