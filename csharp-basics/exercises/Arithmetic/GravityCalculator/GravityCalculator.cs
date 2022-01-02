e659392fd6aee4453562d308675b5495115d77f7
Girts Varna <gvarna@gmail.com>
2022-01-02 13:57:43 +0200

    Arithmetics Exercise07 (GravityCalculator) Tests completed

---
+++ csharp-basics/exercises/Arithmetic/GravityCalculator/GravityCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GravityCalculator
+{
+    public class GravityCalculator
+    {
+        private const double GRAVITY = -9.81;  // Earth's gravity in m/s^2
+        private double _initialVelocity;
+        private double _fallingTime;
+        private double _initialPosition;
+
+        public GravityCalculator()
+        {
+            _initialVelocity = 0.0;
+            _fallingTime = 10.0;
+            _initialPosition = 0.0;
+        }
+
+        public double CalculateFinalPosition()
+        {
+            return 0.5 * GRAVITY * Math.Pow(_fallingTime, 2) + _initialVelocity * _fallingTime + _initialPosition;
+        }
+
+        public string ShowNewObjectPosition()
+        {
+            return "The object's position after " + _fallingTime + " seconds is " + CalculateFinalPosition() + " m.";
+        }
+    }
+}
